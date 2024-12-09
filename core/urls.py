4f1f4e84175e3ccf8f999ed252ea89cb9a355043
ErnestoLeal <ernestolealislazamora@gmail.com>
2024-12-09 06:25:11 +0100

    Version 2.0

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home)
+    path('', views.home, name ='home'),
+    path('experience/', views.experience, name='experience'),
+    path('skills/', views.skills, name='skills'),
+    path('contactme/', views.contactme, name='contactme'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
 def home(request):
     return render(request, 'core/home.html')
+
+def experience(request):
+    return render(request, 'core/experience.html')
+
+def skills(request):
+    return render(request, 'core/skills.html')
+
+def contactme(request):
+    return render(request, 'core/contactme.html')
